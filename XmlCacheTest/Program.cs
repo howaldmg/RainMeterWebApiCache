2d55647d74b4278ad5746e4b5dba06687721bfaf
howaldmg <howaldmg@gmail.com>
2015-09-13 22:26:37 -0400

    quick typo change

--- XmlCacheTest/Program.cs
+++ XmlCacheTest/Program.cs
@@ -6,14 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace ConsoleApplication1
+namespace XmlCacheTest
 {
     class Program
     {
         static void Main(string[] args)
         {
             string results = CacheMan.Query(@"https://api.eveonline.com/server/ServerStatus.xml.aspx");
-            System.Diagnostics.Debugger.Break();
+            
         }
     }
 }
